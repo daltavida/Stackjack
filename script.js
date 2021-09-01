7fb97cb3e6f7f734de48e23b2f0cd2c929398536
Ajay Singh <daltavida@gmail.com>
2021-09-01 14:04:00 +1030

    Reset the game to initial conditions

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-'use strict';
+"use strict";
+
+// Select elements
+const score0El = document.getElementById("score--0");
+const score1El = document.getElementById("score--1");
+
+const diceEl = document.querySelector(".dice");
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
