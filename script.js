02133e7686a0eface0751e192b155026f29b45ee
Ajay Singh <daltavida@gmail.com>
2021-09-02 15:05:00 +1030

    Switch colors on player change

--- script.js
+++ script.js
@@ -16,6 +16,9 @@ const btnHold = document.querySelector(".btn--hold");
 const current0El = document.getElementById("current--0");
 const current1El = document.getElementById("current--1");
 
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+
 // Initial conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -35,5 +38,7 @@ btnRoll.addEventListener("click", function () {
     currentScore = 0;
     document.getElementById(`current--${activePlayer}`).textContent = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
   }
 });
