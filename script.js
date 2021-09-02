63464d12ff0efbcf837dd01cc5e69c43ecda16aa
Ajay Singh <daltavida@gmail.com>
2021-09-02 15:05:00 +1030

    Create functionality to roll a dice

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 "use strict";
 
+// Global variables
+let score1 = 0;
+let score2 = 0;
+
 // Select elements
 const score0El = document.getElementById("score--0");
 const score1El = document.getElementById("score--1");
-
 const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
+// Initial conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
+
+// Rolling dice
+btnRoll.addEventListener("click", function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.src = `dice-${dice}.png`;
+  diceEl.classList.remove("hidden");
+  console.log(dice);
+
+  if (dice !== 1) {
+  } else {
+  }
+});
